72e00b862fe0e46b1ddb7503214b0f03dfb20784
sajalkundu199 <sajalkundu199@gmail.com>
2023-07-14 17:02:26 +0530

    17:02

--- main.tf
+++ main.tf
@@ -60,17 +60,17 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
 
 data "archive_file" "zip_the_python_code" {
  type        = "zip"
- source_dir  = "${path.module}/python/"
- output_path = "${path.module}/python/hello-python.zip"
+ source_dir  = "${path.module}/app/"
+ output_path = "${path.module}/app/hello-app.zip"
 }
 
 # Create a lambda function
 # In terraform ${path.module} is the current directory.
 resource "aws_lambda_function" "terraform_lambda_func" {
- filename                       = "${path.module}/python/hello-python.zip"
+ filename                       = "${path.module}/app/hello-app.zip"
  function_name                  = "Jhooq-Lambda-Function"
  role                           = aws_iam_role.lambda_role.arn
- handler                        = "hello-python.lambda_handler"
+ handler                        = "hello-app.@app.route"
  runtime                        = "python3.8"
  depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
