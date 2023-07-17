b424a43852f4dcd29dc713c4d07e27f967603897
sajalkundu199 <sajalkundu199@gmail.com>
2023-07-17 10:25:29 +0530

    10:25

--- main.tf
+++ main.tf
@@ -5,40 +5,40 @@ provider "aws" {
 }
 
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "chalice"  # Update with your desired bucket name
-  acl    = "private"  # Update with the desired access control list
-}
+# resource "aws_s3_bucket" "my_bucket" {
+#   bucket = "chalice"  # Update with your desired bucket name
+#   acl    = "private"  # Update with the desired access control list
+# }
 
-resource "aws_lambda_function" "my_lambda" {
-  function_name    = "my-lambda"  # Update with your desired Lambda function name
-  role             = aws_iam_role.my_lambda_role.arn
-  handler          = "app.app"
-  runtime          = "python3.8"  # Update with your desired runtime
-  filename         = "lambda.zip"  # Update with the filename of your Lambda deployment package
-  source_code_hash = filebase64sha256("lambda.zip")
-}
+# resource "aws_lambda_function" "my_lambda" {
+#   function_name    = "my-lambda"  # Update with your desired Lambda function name
+#   role             = aws_iam_role.my_lambda_role.arn
+#   handler          = "app.app"
+#   runtime          = "python3.8"  # Update with your desired runtime
+#   filename         = "lambda.zip"  # Update with the filename of your Lambda deployment package
+#   source_code_hash = filebase64sha256("lambda.zip")
+# }
 
-resource "aws_iam_role" "my_lambda_role" {
-  name = "my-lambda-role"  # Update with your desired IAM role name
+# resource "aws_iam_role" "my_lambda_role" {
+#   name = "my-lambda-role"  # Update with your desired IAM role name
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-resource "aws_iam_policy_attachment" "my_lambda_policy_attachment" {
-  name       = "my-lambda-policy-attachment"
-  roles      = [aws_iam_role.my_lambda_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"  # Update with the desired IAM policy ARN
-}
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "lambda.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# EOF
+# }
+# resource "aws_iam_policy_attachment" "my_lambda_policy_attachment" {
+#   name       = "my-lambda-policy-attachment"
+#   roles      = [aws_iam_role.my_lambda_role.name]
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"  # Update with the desired IAM policy ARN
+# }
